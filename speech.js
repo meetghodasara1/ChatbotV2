075cefef2b0011798a6c30b7c8cd0b48ff8d402f
meetghodasara1 <88660996+meetghodasara1@users.noreply.github.com>
2021-11-18 23:18:34 +0530

    Update speech.js

--- speech.js
+++ speech.js
@@ -2,14 +2,26 @@
 
 const synth = window.speechSynthesis;
 
-const textToSpeech = (string) => {
-  let voice = new SpeechSynthesisUtterance(string);
-  let allVoice  = speechSynthesis.getVoices();
- 
-  voice.text = string;
-  voice.lang = "en-US";
-  voice.volume = 1;
-  voice.rate = 1;
-  voice.pitch = 0; // Can be 0, 1, or 2
-  synth.speak(voice);
+// const textToSpeech = (string) => {
+//   let voice = new SpeechSynthesisUtterance(string);
+//   let allVoice  = speechSynthesis.getVoices();
+  
+//   voice.text = string;
+//   voice.lang = "en-US";
+//   voice.volume = 1;
+//   voice.rate = 1;
+//   voice.pitch = 0; // Can be 0, 1, or 2
+//   synth.speak(voice);
+// }
+
+function speak(string) {
+  const u = new SpeechSynthesisUtterance();
+  allVoices = speechSynthesis.getVoices();
+  u.voice = allVoices.filter(voice => voice.name === "Alex")[0];
+  u.text = string;
+  u.lang = "en-US";
+  u.volume = 1; //0-1 interval
+  u.rate = 1;
+  u.pitch = 1; //0-2 interval
+  synth.speak(u);
 }
